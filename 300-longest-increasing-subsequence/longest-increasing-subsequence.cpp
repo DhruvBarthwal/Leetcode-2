6640e21a990b54d16b13339bb9163503b096e03a
Dhruv Barthwal <dhruv4216h@gmail.com>
2025-08-09 19:00:50 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (88.00%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,28 +1,39 @@
 class Solution {
-private:
-    int solve(vector<int>& nums, int n , int i,int prevIndex, vector<vector<int>>&dp){
-        //base case
-        if(i == n){
-            return 0;
-        }
-        if(dp[i][prevIndex+1] != -1){
-            return dp[i][prevIndex+1];
-        }
-        //include and exclude process
-        int skip = solve(nums,n,i+1,prevIndex,dp);
+// private:
+//     int solve(vector<int>& nums, int n , int i,int prevIndex, vector<vector<int>>&dp){
+//         //base case
+//         if(i == n){
+//             return 0;
+//         }
+//         if(dp[i][prevIndex+1] != -1){
+//             return dp[i][prevIndex+1];
+//         }
+//         //include and exclude process
+//         int skip = solve(nums,n,i+1,prevIndex,dp);
 
-        int take = 0;
-        if(prevIndex == -1 || nums[prevIndex]<nums[i]){
-             take = 1+ solve(nums,n,i+1,i,dp);
-        }
-        //return value
-        return dp[i][prevIndex+1] = max(take,skip);
-    }
+//         int take = 0;
+//         if(prevIndex == -1 || nums[prevIndex]<nums[i]){
+//              take = 1+ solve(nums,n,i+1,i,dp);
+//         }
+//         //return value
+//         return dp[i][prevIndex+1] = max(take,skip);
+//     }
 public:
     int lengthOfLIS(vector<int>& nums) {
         //initialization
         int n = nums.size();
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        return solve(nums,n,0,-1,dp);
+        // vector<vector<int>>dp(n,vector<int>(n+1,-1));
+        // return solve(nums,n,0,-1,dp);
+        vector<int>res;
+        for(int i = 0;i<n;i++){
+            auto it = lower_bound(res.begin(),res.end(),nums[i]);
+            if(it == res.end()){
+                res.push_back(nums[i]);
+            }
+            else{
+                *it = nums[i];
+            }
+        }
+        return  res.size();
     }
 };
