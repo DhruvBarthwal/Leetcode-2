a0dab1188db1127beea504f7e587bc74e972d635
Dhruv Barthwal <dhruv4216h@gmail.com>
2025-09-03 19:12:24 +0530

    Time: 3 ms (76.09%) | Memory: 32.5 MB (72.46%) - LeetSync

--- 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
+++ 3278-find-the-number-of-ways-to-place-people-i/find-the-number-of-ways-to-place-people-i.cpp
@@ -1,24 +1,33 @@
 class Solution {
 public:
     int numberOfPairs(vector<vector<int>>& points) {
-        sort(points.begin(), points.end(), [](const auto& a, const auto& b) {
-            if (a[0] == b[0]) return a[1] > b[1]; // y descending if x equal
-            return a[0] < b[0];                   // x ascending
+        //initialization
+        int n = points.size();
+        int count = 0;
+
+        //sorting on the basis of x axis
+        sort(points.begin(), points.end(),[](const vector<int> &a, const vector<int> &b){
+            if(a[0] == b[0]){
+                return a[1] > b[1];
+            }
+            return a[0] < b[0];
         });
 
-        int n = points.size(), result = 0;
-        for (int i = 0; i < n; i++) {
+        //traversing the array
+        for(int i =0;i<n;i++){
             int top = points[i][1];
-            int bot = INT_MIN;
-            for (int j = i + 1; j < n; j++) {
-                int y = points[j][1];
-                if (bot < y && y <= top) {
-                    result++;
-                    bot = y;
-                    if (bot == top) break;
+            int prev = INT_MIN;
+            for(int j =i+1;j<n;j++){
+                int curr = points[j][1];
+                if(prev < curr && curr<=top){
+                    count++;
+                    prev = curr;
+                    if(prev == top){
+                        break;
+                    }
                 }
             }
         }
-        return result;
+        return count;
     }
 };
