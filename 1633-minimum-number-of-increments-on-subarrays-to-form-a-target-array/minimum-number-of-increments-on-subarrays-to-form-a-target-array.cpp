d15585c1a41b7f9bb9fd8a66b6fb6d3b1beea714
Dhruv Barthwal <dhruv4216h@gmail.com>
2025-10-30 10:48:10 +0530

    Time: 2 ms (43.86%) | Memory: 77 MB (28.91%) - LeetSync

--- 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
+++ 1633-minimum-number-of-increments-on-subarrays-to-form-a-target-array/minimum-number-of-increments-on-subarrays-to-form-a-target-array.cpp
@@ -1,22 +1,5 @@
 class Solution {
 public:
-//top down 
-int solve(int i, int n ,int prev ,vector<int>&target, vector<int>&dp){
-    //base case
-    if(i == n) return 0;
-    if(dp[i] != -1) return dp[i];
-
-    int val = target[i] - prev;
-    prev = target[i];
-    if(val > 0){
-        dp[i] = val + solve(i+1,n,prev,target,dp);
-    }
-    else{
-        dp[i] = solve(i+1,n,prev,target,dp);
-    }
-    return dp[i];
-    
-}
     int minNumberOperations(vector<int>& target) {
         int n = target.size();
         int prev = 0, ans = 0;
