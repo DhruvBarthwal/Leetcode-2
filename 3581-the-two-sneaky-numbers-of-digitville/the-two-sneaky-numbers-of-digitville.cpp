39ed5dbefc63a7a47589b7e7b47e85d88005647c
Dhruv Barthwal <dhruv4216h@gmail.com>
2025-10-31 09:45:07 +0530

    Time: 0 ms (100.00%) | Memory: 26.4 MB (15.56%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.cpp
@@ -3,13 +3,13 @@ public:
     vector<int> getSneakyNumbers(vector<int>& nums) {
         vector<int> ans;
         int n = nums.size();
-        unordered_set<int> seen;
+        unordered_map<int,int> seen;
         for(int i = 0;i<n;i++){
-            if(seen.count(nums[i])){
+            if(seen.find(nums[i]) != seen.end()){
                 ans.push_back(nums[i]);
             }
             else{
-                seen.insert(nums[i]);
+                 seen[nums[i]] = i;
             }
         }
         return ans;
