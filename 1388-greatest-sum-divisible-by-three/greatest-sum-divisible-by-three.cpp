12506f73231a5a274e7c94722766b90ffcdc37cc
Dhruv Barthwal <dhruv4216h@gmail.com>
2025-11-23 19:05:13 +0530

    Time: 56 ms (5.88%) | Memory: 46.2 MB (7.31%) - LeetSync

---
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+int n;
+vector<vector<int>> dp;
+int solve(int i ,int r, vector<int> &nums){
+    //base case
+    if(i == n){
+        return(r == 0 ? 0 : INT_MIN);
+    }
+    if(dp[i][r] != -1) return dp[i][r];
+
+    int take = nums[i] + solve(i+1,(r + nums[i])%3,nums);
+    int skip = solve(i+1,r,nums);
+
+    return dp[i][r] = max(take,skip);
+}   
+    int maxSumDivThree(vector<int>& nums) {
+        n = nums.size();
+        dp.assign(n+1,vector<int>(3,-1));
+        return solve(0,0,nums);
+    }
+};
