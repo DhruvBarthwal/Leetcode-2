b2a0cec150450b0d4584101449e20b1453e01b8d
Dhruv Barthwal <dhruv4216h@gmail.com>
2025-11-23 19:29:17 +0530

    Time: 9 ms (68.68%) | Memory: 28.8 MB (84.90%) - LeetSync

--- 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
+++ 1388-greatest-sum-divisible-by-three/greatest-sum-divisible-by-three.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
-int n;
-vector<vector<int>> dp;
+// int n;
+// vector<vector<int>> dp;
 // int solve(int i ,int r, vector<int> &nums){
 //     //base case
 //     if(i == n){
@@ -19,17 +19,32 @@ vector<vector<int>> dp;
 //         dp.assign(n+1,vector<int>(3,-1));
 //         return solve(0,0,nums);
 //     }
+    // int maxSumDivThree(vector<int>& nums) {
+    //     n = nums.size();
+    //     dp.assign(n+1,vector<int>(3,INT_MIN));
+    //     dp[n][0] = 0;
+    //     for(int i = n-1;i>=0;i--){
+    //         for(int r = 0;r<3;r++){
+    //             int take = nums[i] + dp[i+1][(r+nums[i])%3];
+    //             int skip = dp[i+1][r];
+    //             dp[i][r] = max(take,skip); 
+    //         }
+    //     }
+    //     return dp[0][0];
+    // }
     int maxSumDivThree(vector<int>& nums) {
-        n = nums.size();
-        dp.assign(n+1,vector<int>(3,INT_MIN));
-        dp[n][0] = 0;
+        int n = nums.size();
+        vector<int>prev(3,INT_MIN), curr(3,INT_MIN);
+        prev[0] = 0;
         for(int i = n-1;i>=0;i--){
+            curr = prev;
             for(int r = 0;r<3;r++){
-                int take = nums[i] + dp[i+1][(r+nums[i])%3];
-                int skip = dp[i+1][r];
-                dp[i][r] = max(take,skip); 
+                int take = nums[i] + prev[(r+nums[i])%3];
+                int skip = prev[r];
+                curr[r] = max(take,skip); 
             }
+            prev = curr;
         }
-        return dp[0][0];
+        return prev[0];
     }
 };
