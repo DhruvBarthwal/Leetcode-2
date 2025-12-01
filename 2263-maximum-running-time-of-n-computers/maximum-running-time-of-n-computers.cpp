7e40d978d6d64a8d6cc3bc44f5dfbbde8ddf1aec
Dhruv Barthwal <dhruv4216h@gmail.com>
2025-12-01 14:38:33 +0530

    Time: 27 ms (44.27%) | Memory: 59.8 MB (28.34%) - LeetSync

---
+++ 2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    long long maxRunTime(int n, vector<int>& batteries) {
+        long long total = accumulate(batteries.begin(),batteries.end(),0LL);
+        sort(batteries.begin(),batteries.end());
+
+        int m = batteries.size();
+        for(int i = m-1;i>=0;i--){
+            if(batteries[i] > total/n){
+                total -= batteries[i];
+                n--;
+            }
+            else break;
+        }
+        return total/n;
+    }
+};
