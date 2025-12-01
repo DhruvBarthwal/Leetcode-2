957ee4b98a9395bd64f08c86b616daf16358e829
Dhruv Barthwal <dhruv4216h@gmail.com>
2025-12-01 16:38:33 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (52.59%) - LeetSync

--- 2412-minimum-amount-of-time-to-fill-cups/minimum-amount-of-time-to-fill-cups.cpp
+++ 2412-minimum-amount-of-time-to-fill-cups/minimum-amount-of-time-to-fill-cups.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     int fillCups(vector<int>& amount) {
-        int count = 0;
-        priority_queue<int> pq;
-        for(int num : amount) pq.push(num);
-        while(pq.top() != 0){
-            int a = pq.top();
-            pq.pop();
-            int b = pq.top();
-            pq.pop();
-            a--;
-            b--;
-            pq.push(a);
-            pq.push(b);
-            count++;
-        }
-        return count;
+        // int count = 0;
+        // priority_queue<int> pq;
+        // for(int num : amount) pq.push(num);
+        // while(pq.top() != 0){
+        //     int a = pq.top();
+        //     pq.pop();
+        //     int b = pq.top();
+        //     pq.pop();
+        //     a--;
+        //     b--;
+        //     pq.push(a);
+        //     pq.push(b);
+        //     count++;
+        // }
+        // return count;
+        int mx = *max_element(amount.begin(),amount.end());
+        int sum = accumulate(amount.begin(),amount.end(), 0LL);
+        return max(mx, (sum+1)/2);
     }
 };
