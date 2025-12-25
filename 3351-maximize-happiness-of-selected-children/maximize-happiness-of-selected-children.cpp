c7d284962a583567462f576e0d7f1c9302fc11da
Dhruv Barthwal <dhruv4216h@gmail.com>
2025-12-25 13:58:39 +0530

    Time: 214 ms (17.38%) | Memory: 110.2 MB (18.09%) - LeetSync

--- 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
+++ 3351-maximize-happiness-of-selected-children/maximize-happiness-of-selected-children.cpp
@@ -3,17 +3,13 @@ public:
     long long maximumHappinessSum(vector<int>& happiness, int k) {
         int n = happiness.size();
         sort(happiness.rbegin(),happiness.rend());
-        queue<long long> q;
-
-        for(int num : happiness) q.push(num);
-
         long long subcount = 0, happy = 0;
-        while(!q.empty() && k>0){
-            long long value = q.front();
-            q.pop();
-            if(value >= subcount) happy += (value - subcount);
+        int i = 0;
+        while(i<n && k--){
+            long long value = happiness[i];
+            if(value >= subcount) happy += value - subcount;
             subcount++;
-            k--;
+            i++;
         }
         return happy;
     }
