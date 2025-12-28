8573a36ee55362ee4b2e7b8d6fc0f552cf125f4f
Dhruv Barthwal <dhruv4216h@gmail.com>
2025-12-28 10:52:56 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (64.29%) - LeetSync

--- 4147-minimum-cost-to-acquire-required-items/minimum-cost-to-acquire-required-items.cpp
+++ 4147-minimum-cost-to-acquire-required-items/minimum-cost-to-acquire-required-items.cpp
@@ -1,14 +1,22 @@
 class Solution {
 public:
+//Disclaimer!!! = Understand logic by ignoring 1LL
+//1LL is used to nulify overflow
     long long minimumCost(int cost1, int cost2, int costBoth, int need1, int need2) {
+        // Running cost : Sum of both cost individually
         long long runningCost = 1LL*(cost1 + cost2);
+
         long long val = min(runningCost,(long long)costBoth);
+
+        //if need1 is greater : sum of (cost till need1 == need2) + (remaining need1 cost)
         if(need1 > need2){
-            return 1LL * need2 * val + 1LL * min(cost1,costBoth) *(need1 - need2);
+            return (1LL * need2 * val) + (1LL * min(cost1,costBoth) *(need1 - need2));
         }
+        //if need2 is greater : sum of (cost till need1 == need2) + (remaining need2 cost)
         else if(need2 > need1){
-            return 1LL * need1 * val + 1LL * min(cost2,costBoth) *(need2 - need1);
+            return (1LL * need1 * val + 1LL * min(cost2,costBoth) *(need2 - need1));
         }
-        return 1ll * need1 * val;
+        //if need1 == need2
+        return (1LL * need1 * val);
     }
 };
