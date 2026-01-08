a6a77d4c2c048479bd6907ad06b27db489d6985f
Dhruv Barthwal <dhruv4216h@gmail.com>
2026-01-08 11:54:29 +0530

    Time: 11 ms (88.40%) | Memory: 21.1 MB (8.40%) - LeetSync

--- 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.cpp
+++ 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.cpp
@@ -1,21 +1,40 @@
 class Solution {
 public:
-vector<vector<long long>> dp;
-int n,m;
-long long solve(int i, int j, vector<int>&nums1 , vector<int>& nums2){
-    if(i == n || j == m) return LLONG_MIN;
-    if(dp[i][j] != -1) return dp[i][j];
+// CASE 1 : Top Down Approach
+// vector<vector<long long>> dp;
+// int n,m;
+// long long solve(int i, int j, vector<int>&nums1 , vector<int>& nums2){
+//     if(i == n || j == m) return LLONG_MIN;
+//     if(dp[i][j] != -1) return dp[i][j];
 
-    long long take = (nums1[i] * nums2[j]) + max(0LL, solve(i+1,j+1, nums1,nums2));
-    long long skipI = solve(i+1,j, nums1, nums2);
-    long long skipJ = solve(i,j+1, nums1, nums2);
+//     long long take = (nums1[i] * nums2[j]) + max(0LL, solve(i+1,j+1, nums1,nums2));
+//     long long skipI = solve(i+1,j, nums1, nums2);
+//     long long skipJ = solve(i,j+1, nums1, nums2);
 
-    return dp[i][j] = max({take,skipI,skipJ});
-}
+//     return dp[i][j] = max({take,skipI,skipJ});
+// }
+    // int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
+    //     n = nums1.size();
+    //     m = nums2.size();
+    //     dp.assign(n+1,vector<long long>(m+1,-1));
+    //     return solve(0,0,nums1,nums2);
+    // }
+
+//CASE 2 : BOTTOM UP APPROACH
+    vector<vector<long long>> dp;
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
-        n = nums1.size();
-        m = nums2.size();
-        dp.assign(n+1,vector<long long>(m+1,-1));
-        return solve(0,0,nums1,nums2);
+        int n = nums1.size();
+        int m = nums2.size();
+        dp.assign(n+1,vector<long long>(m+1,LLONG_MIN));
+        
+        for(int i = n-1;i>=0;i--){
+            for(int j = m-1;j>=0;j--){
+                long long take = (nums1[i] * nums2[j]) + max(0LL, dp[i+1][j+1]);
+                long long skip1 = dp[i+1][j];
+                long long skip2 = dp[i][j+1];
+                dp[i][j] = max({take,skip1,skip2});
+            }
+        }
+        return dp[0][0];
     }
 };
