e4880452e0a81bf0d5cd106aa8ad07d41931f435
Dhruv Barthwal <dhruv4216h@gmail.com>
2026-02-03 12:03:46 +0530

    Time: 0 ms (100.00%) | Memory: 27.7 MB (35.38%) - LeetSync

--- 3952-trionic-array-i/trionic-array-i.cpp
+++ 3952-trionic-array-i/trionic-array-i.cpp
@@ -4,17 +4,19 @@ public:
         int n = nums.size();
         int count = 0;
         int i = 0;
+        //if starting valus decreasing or equal
         if(nums[i] > nums[i+1] || nums[i] == nums[i+1]) return false;
+
         while(i<n-1){
-            if(nums[i] < nums[i+1]){
+            if(nums[i] < nums[i+1]){    //Strictly increasing
                 count++;
                 while(i < n-1 && nums[i] < nums[i+1]) i++;
             }
-            else if (nums[i] > nums[i+1]){
+            else if (nums[i] > nums[i+1]){ //Strictly decreasing
                 count++;
                 while(i< n-1 && nums[i] > nums[i+1]) i++;
             }
-            else  return false;
+            else  return false;   //equal values
         }
         return count == 3;
     }
