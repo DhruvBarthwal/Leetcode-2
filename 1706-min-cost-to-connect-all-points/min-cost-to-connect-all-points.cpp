6790404bbbb819492c13dc43579738d8dd26cb14
Dhruv Barthwal <dhruv4216h@gmail.com>
2026-02-10 00:07:54 +0530

    Time: 437 ms (45.44%) | Memory: 134.7 MB (37.74%) - LeetSync

--- 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
     int minCostConnectPoints(vector<vector<int>>& points) {
-        unordered_map<int, vector<pair<int,int>>> adj;
         int n = points.size();
-        for(int i = 0;i<n;i++){
+        vector<vector<pair<int,int>>> adj(n);
+        for(int i  = 0;i<n-1;i++){
             for(int j =i+1;j<n;j++){
                 int w = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]);
                 adj[i].push_back({j,w});
                 adj[j].push_back({i,w});
             }
         }
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>>pq;
-        int mstWeight = 0;
-        int visCount = 0;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>pq;
         vector<bool> vis(n,false);
+        int mst = 0;
         pq.push({0,0});
         while(!pq.empty()){
-            auto [w,u] = pq.top();
+            auto [weight, u] = pq.top();
             pq.pop();
             if(vis[u]) continue;
+            mst += weight;
             vis[u] = true;
-            visCount++;
-            mstWeight += w;
-            for(auto [v,weight] : adj[u]){
+            for(auto &[v,w] : adj[u]){
                 if(!vis[v]){
-                    pq.push({weight,v});
+                    pq.push({w,v});
                 }
             }
         }
-        if(visCount != n) return -1;
-        return mstWeight;
+        return mst;
     }
 };
