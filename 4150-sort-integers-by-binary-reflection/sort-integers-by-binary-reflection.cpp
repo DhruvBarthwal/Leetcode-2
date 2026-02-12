ddcae84c323e96bb0c395d993258fedc030a8168
Dhruv Barthwal <dhruv4216h@gmail.com>
2026-02-13 02:02:52 +0530

    Time: 88 ms (9.29%) | Memory: 33.5 MB (59.81%) - LeetSync

---
+++ 4150-sort-integers-by-binary-reflection/sort-integers-by-binary-reflection.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> sortByReflection(vector<int>& nums) {
+        int n = nums.size();
+        priority_queue<pair<int,int>,vector<pair<int,int>>, greater<>> pq;
+        vector<int> ans;
+        for(int i = 0;i<n;i++){
+            int val = 0;
+            int temp = nums[i];
+            while(temp > 0){
+                val = (val << 1) | (temp & 1);
+                temp >>= 1;
+            }
+            cout<<val<<" "<<temp<<" ";
+            pq.push({val,nums[i]});
+        }
+        while(!pq.empty()){
+            auto [x,y] = pq.top();
+            pq.pop();
+            ans.push_back(y);
+        }
+        return ans;
+    }
+};
