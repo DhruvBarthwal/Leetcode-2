0834e063a666da5c49859997213de9d21af94e6c
Dhruv Barthwal <dhruv4216h@gmail.com>
2026-02-24 00:01:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (52.24%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
-int solve(int i, int j, int n, int m){
-    //base case
-    if(i == n-1 && j == m-1) return 1;
-    if(i >= n || j >= m) return 0;
+// int solve(int i, int j, int n, int m){
+//     //base case
+//     if(i == n-1 && j == m-1) return 1;
+//     if(i >= n || j >= m) return 0;
 
-    if(dp[i][j] != -1) return dp[i][j];
+//     if(dp[i][j] != -1) return dp[i][j];
 
-    int right = 0, bottom = 0;;
-    if(j < m) right = solve(i,j+1,n,m);
-    if(i < n) bottom = solve(i+1,j,n,m);
+//     int right = 0, bottom = 0;;
+//     if(j < m) right = solve(i,j+1,n,m);
+//     if(i < n) bottom = solve(i+1,j,n,m);
 
-    return dp[i][j] = right + bottom;
-}
-    vector<vector<int>>dp;
+//     return dp[i][j] = right + bottom;
+// }
+    // vector<vector<int>>dp;
+    // int uniquePaths(int m, int n) {
+    // dp.assign(m,vector<int>(n,-1));
+    //    return solve(0,0,m,n); 
+    // }
     int uniquePaths(int m, int n) {
-    dp.assign(m,vector<int>(n,-1));
-       return solve(0,0,m,n); 
+        vector<vector<int>>dp(m,vector<int>(n,1));
+        for(int i =1;i<m;i++){
+            for(int j = 1;j<n;j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
