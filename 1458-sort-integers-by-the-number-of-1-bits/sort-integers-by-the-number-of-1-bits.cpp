2386980e385bce4633dd90d44bab8d670a258a46
Dhruv Barthwal <dhruv4216h@gmail.com>
2026-02-25 17:12:23 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (43.16%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     vector<int> sortByBits(vector<int>& arr) {
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
-        int n = arr.size();
-        for(int i =0;i<n;i++){
-            int val = __builtin_popcount(arr[i]);
-            pq.push({val,arr[i]});
-        }
-        vector<int> ans;
-        while(!pq.empty()){
-            auto [val,num] = pq.top();
-            pq.pop();
-            ans.push_back(num);
-        }
-        return ans;
+        sort(arr.begin(),arr.end(),[](int a, int b){
+            int x = __builtin_popcount(a);
+            int y = __builtin_popcount(b);
+            if(x == y) return a<b;
+            return x < y;
+        });
+        return arr;
     }
 };
