6617849bb4ba5b47783de43ea2f192c1d2632572
Dhruv Barthwal <dhruv4216h@gmail.com>
2026-05-15 03:32:35 +0530

    Time: 0 ms (100.00%) | Memory: 26.3 MB (85.57%) - LeetSync

---
+++ 2892-check-if-array-is-good/check-if-array-is-good.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isGood(vector<int>& nums) {
+        int n = nums.size();
+        int maxi = *max_element(nums.begin(),nums.end());
+        if(n != maxi + 1) return false;
+        sort(nums.begin(),nums.end());
+        for(int i =0;i<n-1;i++) if(nums[i] != i+1) return false;
+        if(nums[n-1] != maxi) return false;
+        return true;
+    }
+};
