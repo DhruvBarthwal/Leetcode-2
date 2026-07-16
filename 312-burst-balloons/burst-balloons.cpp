7491686883de3c55d6b683b081ddf20be3606b25
Dhruv Barthwal <dhruv4216h@gmail.com>
2026-07-17 00:35:06 +0530

    Time: 214 ms (11.07%) | Memory: 14 MB (31.08%) - LeetSync

--- 312-burst-balloons/burst-balloons.cpp
+++ 312-burst-balloons/burst-balloons.cpp
@@ -1,22 +1,29 @@
 class Solution {
 public:
-int solve(int i, int j, vector<int> &nums,vector<vector<int>>&dp){
-    //base case
-    if(i > j) return 0;
-    if(dp[i][j] != -1 ) return dp[i][j];
+    //global variable
+    int n;
+    vector<vector<int>> dp;
+    int solve(int i , int j, vector<int> &nums){
+        //base case
+        if(i > j) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
 
-    int maxi = 0;
-    for(int k = i;k<=j;k++){
-        int ans = nums[i-1] * nums[k] * nums[j+1] + solve(i,k-1,nums,dp) + solve(k+1,j,nums,dp);
-        maxi = max(ans,maxi);
+        int ans = 0;
+        for(int k = i;k<=j;k++){
+            int mini = nums[i-1]*nums[k]*nums[j+1] + solve(i,k-1,nums) + solve(k+1,j,nums);
+            ans = max(ans,mini);
+        }
+        return dp[i][j] = ans;
     }
-    return dp[i][j] = maxi;
-}
+
     int maxCoins(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> dp(n+2,vector<int>(n+2,-1));
-        nums.push_back(1);
-        nums.insert(nums.begin(), 1);
-        return solve(1,n,nums,dp);
+       //initialization
+       n = nums.size();
+       dp.assign(n+2,vector<int>(n+2,-1));
+       
+       nums.push_back(1);
+       nums.insert(nums.begin(),1);
+
+       return solve(1,n,nums); 
     }
 };
