fc21293a9cca401266f63e153cb1a9c4ee0b9bfd
Dhruv Barthwal <dhruv4216h@gmail.com>
2026-07-16 17:12:04 +0530

    Time: 31 ms (93.57%) | Memory: 18.7 MB (69.34%) - LeetSync

--- 1129-longest-string-chain/longest-string-chain.cpp
+++ 1129-longest-string-chain/longest-string-chain.cpp
@@ -1,45 +1,51 @@
 class Solution {
 public:
+    //global variable
+    vector<int> dp;
     int n;
-    vector<int>dp;
-    bool isSubsequence(string a, string b){
-        if(a.length() + 1 != b.length()) return false;
-        int i = 0, j  = 0;
-        while(i<a.length() && j<b.length()){
-            if(a[i] != b[j]){
-                j++;
-            }
+
+    bool isSubsequence(string&a, string&b){
+        int i = 0, j =0;
+        while(i < a.size() && j < b.size()){
+            if(a[i] != b[j]) j++;
             else{
                 i++;
                 j++;
             }
         }
-        return i == a.length();
+        return i == a.size();
     }
-    int solve(int i, vector<string>& words){
+
+    int solve(int i, vector<string>&words){
         //base case
-        if(i == n) return 0;
+        if(i >= n) return 0;
         if(dp[i] != -1) return dp[i];
 
-        int best = 1;
+        int ans = 1;
+
         for(int j = i+1;j<n;j++){
             if(words[i].size() + 1 == words[j].size() && isSubsequence(words[i],words[j])){
-                best = max(best, 1+ solve(j,words));
+                ans = max(ans,1 + solve(j,words));
             }
         }
-        return dp[i] = best;
 
+        return dp[i] = ans;
     }
+
     int longestStrChain(vector<string>& words) {
+        //initialization
         n = words.size();
-        dp.assign(n+1,-1);
-        sort(words.begin(),words.end(),[](auto &a, auto &b){
+        dp.assign(n,-1);
+
+        //sort the array
+        sort(words.begin(),words.end(),[](string &a , string&b){
             return a.size() < b.size();
         });
+
         int ans = 1;
-        for(int i = 0;i<n;i++){
+        for(int i =0;i<n;i++){
             ans = max(ans,solve(i,words));
         }
-        return ans; 
+        return ans;
     }
 };
