5f646923436a858f45b38d887d9ce32c591acb64
Dhruv Barthwal <dhruv4216h@gmail.com>
2026-08-05 03:26:40 +0530

    Time: 1596 ms (23.07%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1480-movie-rating/movie-rating.sql
@@ -0,0 +1,36 @@
+# Write your MySQL query statement below
+(SELECT 
+    name as results
+FROM
+    MovieRating
+JOIN
+    Users
+USING
+    (user_id)
+GROUP BY
+    name
+ORDER BY 
+    COUNT(*) DESC, name
+LIMIT 1)
+
+UNION ALL
+
+(
+SELECT
+    title as results
+FROM
+    MovieRating
+JOIN
+    Movies
+USING
+    (movie_id)
+WHERE
+    created_at BETWEEN '2020-02-01' AND '2020-02-29'
+GROUP BY
+    title
+ORDER BY
+    AVG(rating) DESC, title
+LIMIT 1
+)
+
+
