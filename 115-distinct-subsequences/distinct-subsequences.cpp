2290d8259cf1a59a5d315a19039114582761fbe1
Dhruv Barthwal <dhruv4216h@gmail.com>
2026-09-06 12:54:23 +0530

    Time: 19 ms (79.93%) | Memory: 8.8 MB (95.82%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -31,7 +31,34 @@ public:
     //     return solve(0,0,s,t);
     // }
 
-        int numDistinct(string s, string t) {
+//         int numDistinct(string s, string t) {
+//         //initialization
+//         n = s.length();
+//         m = t.length();
+
+//         //edge case
+//         if(n < m) return 0;
+
+//         dp.assign(n+1,vector<long long>(m+1,0));
+
+//         for(int i = 0;i<=n;i++){
+//             dp[i][m] = 1;
+//         }
+
+//         for(int i = n-1;i>=0;i--){
+//             for(int j =m-1;j>=0;j--){
+//                 dp[i][j] = dp[i+1][j];
+                
+//                 if(s[i] == t[j]) dp[i][j] += dp[i+1][j+1];
+
+//                 if(dp[i][j] > INT_MAX) dp[i][j] = INT_MAX;
+//             }
+//         }
+//         return dp[0][0];
+//     }
+// };
+
+    int numDistinct(string s, string t) {
         //initialization
         n = s.length();
         m = t.length();
@@ -39,21 +66,24 @@ public:
         //edge case
         if(n < m) return 0;
 
-        dp.assign(n+1,vector<long long>(m+1,0));
+        vector<long long>curr(m+1,0);
+        vector<long long>next(m+1,0);
 
-        for(int i = 0;i<=n;i++){
-            dp[i][m] = 1;
-        }
+        next[m] = 1;
 
         for(int i = n-1;i>=0;i--){
+            curr.assign(m+1,0);
+            curr[m] = 1;
             for(int j =m-1;j>=0;j--){
-                dp[i][j] = dp[i+1][j];
+                curr[j] = next[j];
                 
-                if(s[i] == t[j]) dp[i][j] += dp[i+1][j+1];
+                if(s[i] == t[j]) curr[j] += next[j+1];
 
-                if(dp[i][j] > INT_MAX) dp[i][j] = INT_MAX;
+                if(curr[j] > INT_MAX) curr[j] = INT_MAX;
+
             }
+            next = curr;
         }
-        return dp[0][0];
+        return next[0];
     }
 };
