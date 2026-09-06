ca4c15185f764afb642d324f77db7034a15ef0a5
Dhruv Barthwal <dhruv4216h@gmail.com>
2026-09-06 12:43:34 +0530

    Time: 30 ms (58.87%) | Memory: 44.5 MB (11.48%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
     //global variables
-    vector<vector<int>>dp;
+    vector<vector<long long>>dp;
     int n, m;
 
-    int solve(int i, int j, string&s, string&t){
-        //base case
-        if(j == m) return 1;
-        if(i == n && j < m) return 0;
+    // int solve(int i, int j, string&s, string&t){
+    //     //base case
+    //     if(j == m) return 1;
+    //     if(i == n && j < m) return 0;
 
-        if(dp[i][j] != -1) return dp[i][j];
+    //     if(dp[i][j] != -1) return dp[i][j];
 
-        int skip = solve(i+1,j,s,t);
-        int take = 0;
-        if(s[i] == t[j]) take = solve(i+1,j+1,s,t);
+    //     int skip = solve(i+1,j,s,t);
+    //     int take = 0;
+    //     if(s[i] == t[j]) take = solve(i+1,j+1,s,t);
 
-        return dp[i][j] = take + skip; 
-    }
+    //     return dp[i][j] = take + skip; 
+    // }
 
-    int numDistinct(string s, string t) {
+    // int numDistinct(string s, string t) {
+    //     //initialization
+    //     n = s.length();
+    //     m = t.length();
+
+    //     //edge case
+    //     if(n < m) return 0;
+
+    //     dp.assign(n+1,vector<int>(m+1,-1));
+
+    //     return solve(0,0,s,t);
+    // }
+
+        int numDistinct(string s, string t) {
         //initialization
         n = s.length();
         m = t.length();
@@ -26,8 +39,21 @@ public:
         //edge case
         if(n < m) return 0;
 
-        dp.assign(n+1,vector<int>(m+1,-1));
+        dp.assign(n+1,vector<long long>(m+1,0));
 
-        return solve(0,0,s,t);
+        for(int i = 0;i<=n;i++){
+            dp[i][m] = 1;
+        }
+
+        for(int i = n-1;i>=0;i--){
+            for(int j =m-1;j>=0;j--){
+                dp[i][j] = dp[i+1][j];
+                
+                if(s[i] == t[j]) dp[i][j] += dp[i+1][j+1];
+
+                if(dp[i][j] > INT_MAX) dp[i][j] = INT_MAX;
+            }
+        }
+        return dp[0][0];
     }
 };
