ce87986f6033e42b444ffc8c73eef287ad5ac433
Dhruv Barthwal <dhruv4216h@gmail.com>
2026-09-15 20:18:27 +0530

    Time: 23 ms (83.32%) | Memory: 16.6 MB (64.15%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,35 +1,43 @@
 class Solution {
 public:
-    //global variables
-    int n, m;
-    vector<vector<bool>> vis;
-    vector<pair<int,int>> dis = {{0,1},{1,0},{0,-1},{-1,0}};
+//global variable
+int rows , cols;
+vector<vector<bool>> vis;  //visted 
+vector<pair<int,int>> dir = {{0,1},{1,0},{-1,0},{0,-1}}; //direction
 
-    void dfs(int i, int j, vector<vector<char>>&grid){
-        if(grid[i][j] == '0') return;
-        vis[i][j] = true;
+    void dfs(int row, int col, vector<vector<char>>&grid){
+        //base case
+        if(grid[row][col] == '0') return;
+        vis[row][col] = true;
 
-        for(auto &[dx,dy] : dis){
-            int x = i + dx, y = j + dy;
-            if(x < 0 || x >=n || y<0 || y >=m) continue;
-            if(!vis[x][y]) dfs(x,y,grid);
+        for(auto &[dx,dy] : dir){
+            int newRow = row + dx;
+            int newCol = col + dy;
+            //handling overflow
+            if(newRow < 0 || newCol < 0 || newRow >= rows || newCol >= cols) continue;
+
+            if(!vis[newRow][newCol]) dfs(newRow,newCol,grid);
         }
     }
 
     int numIslands(vector<vector<char>>& grid) {
-        n = grid.size(), m = grid[0].size();
-        vis.assign(n,vector<bool>(m,false));
-        int cnt = 0;
+        //intialization
+        rows = grid.size(), cols = grid[0].size();
+        vis.assign(rows,vector<bool>(cols,false));
+        int islands = 0;
 
-        for(int i =0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                if(grid[i][j] == '1' && !vis[i][j]){
-                    dfs(i,j,grid);
-                    cnt++;
+        //traverse the loop
+        for(int row = 0;row< rows;row++){
+            for(int col =0;col< cols;col++){
+                if(!vis[row][col] && grid[row][col] == '1'){
+                    //dfs
+                    dfs(row,col,grid);
+                    islands++;
                 }
             }
         }
 
-        return cnt;
+        //return ans
+        return islands;
     }
 };
