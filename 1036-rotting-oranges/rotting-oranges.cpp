eb9e8185a9fb7f5ce168fb1712b2b2681fb514f0
Dhruv Barthwal <dhruv4216h@gmail.com>
2026-09-15 19:56:35 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (16.79%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,37 +1,45 @@
 class Solution {
 public:
-int n, m;
+
     int orangesRotting(vector<vector<int>>& grid) {
-        n = grid.size(), m = grid[0].size();
-        int time = 0;
-        int cnt =0;
-        queue<tuple<int,int,int>> q;
-        vector<vector<bool>> vis(n,vector<bool>(m,false));
-        for(int i = 0;i<n;i++){
-            for(int j = 0;j<m;j++){
-                if(grid[i][j] == 2){
-                    q.push({i,j,0});
-                    vis[i][j] = true;
-                }
-                else if(grid[i][j] == 1) cnt++;
+        //intialization
+        int rows = grid.size(), cols = grid[0].size();
+        int time = 0, cntFreshOranges = 0, finalTime = 0;
+        queue<tuple<int,int,int>> q; //row, col, time
+        vector<pair<int,int>> dir = {{1,0},{0,1},{-1,0},{0,-1}}; //directions
+        vector<vector<bool>> vis(rows,vector<bool>(cols,false));
+
+        //traverse the grid
+        for(int row =0;row<rows;row++){
+            for(int col =0;col<cols;col++){
+                if(grid[row][col] == 1) cntFreshOranges++;
+                else if(grid[row][col] == 2) q.push({row,col,0});
             }
         }
-        if(cnt == 0) return 0;
-        vector<pair<int,int>> dis = {{0,1},{1,0},{-1,0},{0,-1}};
+
+        //edge case
+        if(cntFreshOranges == 0) return 0;
+
+        //bfs
         while(!q.empty()){
-            auto [x,y,t] = q.front();
+            auto [i,j,t] = q.front(); // currRow, currCol, currTime
             q.pop();
-            time = max(time,t);
-            for(auto &[dx,dy] : dis){
-                int i = x + dx;
-                int j = y + dy;
-                if(i >= 0 && j >= 0 && i <n && j< m && vis[i][j] == false && grid[i][j] == 1){
-                    cnt--;
-                    vis[i][j] = true;
-                    q.push({i,j,t+1});
-                }
+            
+            finalTime = max(finalTime, t);
+
+            for(auto &[dx,dy] : dir){
+                int newRow = i + dx;
+                int newCol = j + dy;
+
+                if(newRow < 0 || newCol < 0 || newRow >= rows || newCol >= cols || grid[newRow][newCol] != 1) continue;
+                if(vis[newRow][newCol]) continue;
+
+                cntFreshOranges--;
+                vis[newRow][newCol] = true;
+
+                q.push({newRow,newCol,t+1});
             }
         }
-        return (cnt == 0) ? time : -1;
+        return cntFreshOranges == 0 ? finalTime : -1;
     }
 };
