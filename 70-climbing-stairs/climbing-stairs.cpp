754057981467788448a8090ba0575e2415465b3f
Dhruv Barthwal <dhruv4216h@gmail.com>
2026-09-16 17:18:02 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (58.93%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,12 +1,29 @@
 class Solution {
 public:
+
+// 1 orr 2
+//no. of d
+
+//1 - 0
+//2 - 0 , 1
+    // vector<int> dp;
+    // int solve(int n){
+    //     //base case
+    //     if(n == 0 || n == 1)  return 1;
+    //     if(dp[n] != -1) return dp[n];
+
+    //     return dp[n] = solve(n-1) + solve(n-2);
+    // }
+
     int climbStairs(int n) {
-        int prev = 1 , curr = 1;
-        for(int i =2;i<=n;i++){
-            int val = prev + curr;
-            prev = curr;
-            curr = val;
+        int prev1 = 1, prev2 = 1;
+        for(int i = 2;i<=n;i++){
+            int curr = prev1 + prev2;
+            prev1 = prev2;
+            prev2 = curr;
+
         }
-        return curr;
+
+        return prev2;
     }
 };
